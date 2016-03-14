ef1df53a3bb87c6df7ea2d6e01bca240a5c90fc8
kickscar <kickscar@gmail.com>
2016-03-14 15:07:11 +0900

    1st commit

---
+++ src/prob1/Game369.java
@@ -0,0 +1,6 @@
+package prob1;
+
+public class Game369 {
+	public static void main(String[] args) {
+	}
+}
---
+++ src/prob2/CalcApp.java
@@ -0,0 +1,8 @@
+package prob2;
+
+public class CalcApp {
+
+	public static void main(String[] args) {
+	}
+
+}
